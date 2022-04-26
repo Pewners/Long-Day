e5e5edb78fa8df1f6b6871092d9fe22132eca3b1
Pewners <jfadikpe2021@elam.co.uk>
2022-04-26 12:35:58 +0100

    post processing works bro

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Pickup : MonoBehaviour
 {
+    public int type = 0;
+
     private void OnTriggerEnter(Collider other)
     {
         //insert code that adds to counter
-        Destroy(gameObject);
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+
+            if(type == 0)
+            {
+                PostProcessVolume grain = GameObject.Find("GrainPP").GetComponent<PostProcessVolume>();
+
+                grain.enabled = true;
+            }
+        }
+
+
     }
 
     // Update is called once per frame
