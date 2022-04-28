6bc918993cd6579b042b5d0478dea274bbfe6001
Pewners <jfadikpe2021@elam.co.uk>
2022-04-28 15:00:55 +0100

    skrunkly door

--- Assets/Scripts/DoorOpenController.cs
+++ Assets/Scripts/DoorOpenController.cs
@@ -5,25 +5,70 @@ using UnityEngine.SceneManagement;
 
 public class DoorOpenController : MonoBehaviour
 {
-    public GameObject instructions;
-    private void OnTriggerStay(Collider other)
+//    public GameObject instructions;
+//    private void OnTriggerStay(Collider other)
+//    {
+//        if (other.tag == "Door2")
+//        {
+//            instructions.SetActive(true);
+//            if (Input.GetKeyDown(KeyCode.E))
+//            {
+//                Animator anim = other.GetComponentInChildren<Animator>();
+//                anim.SetTrigger("OpenClose");
+//            }
+//        }
+//    }
+
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.tag == "Door2")
+//        {
+//            instructions.SetActive(false);
+//        }
+//    }
+
+public GameObject instructions;
+    Animator anim;
+    bool inTrigger = false;
+    bool OpenClose = false;
+
+    void Start()
     {
-        if (other.tag == "Door2")
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (inTrigger == true) 
         {
-            instructions.SetActive(true);
+            //Debug.Log("in trigger");
+
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Animator anim = other.GetComponentInChildren<Animator>();
-                anim.SetTrigger("OpenClose");
+                Debug.Log("e pressed");
+
+                OpenClose = !OpenClose;
             }
+
+        }
+
+        anim.SetBool("OpenClose", OpenClose);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            instructions.SetActive(true);
+            inTrigger = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Door2")
+        if (other.tag == "Player")
         {
             instructions.SetActive(false);
+            inTrigger = false;
         }
     }
 }
