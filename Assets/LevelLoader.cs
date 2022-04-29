2084e892b1aba26c8db0233ddeed6cd4f6271ec7
Pewners <jfadikpe2021@elam.co.uk>
2022-04-29 11:52:13 +0100

    FUCK

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
@@ -12,5 +13,10 @@ public class LevelLoader : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LoadLevel()
+    {
+        Scene
     }
 }
