034783184185fa2130de6a431eaf360923ed60d7
Pewners <jfadikpe2021@elam.co.uk>
2022-04-29 01:28:03 +0100

    working doors and tried making scene transition but flopped

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
