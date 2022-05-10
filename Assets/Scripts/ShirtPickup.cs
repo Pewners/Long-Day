93bb0c90681bd189d68b137d7ad604f59747c6f3
Pewners <jfadikpe2021@elam.co.uk>
2022-05-10 12:41:44 +0100

    started on minigame ting

--- Assets/Scripts/ShirtPickup.cs
+++ Assets/Scripts/ShirtPickup.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class ShirtPickup : MonoBehaviour
 {
+    public int score;
+
     private void OnTriggerEnter(Collider other)
     {
         //insert code that adds to counter
         if (other.tag == "Player")
         {
             Destroy(gameObject);
+            score++;
+        }
+    }
+
+    private void Update()
+    {
+        if (score > 3)
+        {
+
         }
     }
 }
